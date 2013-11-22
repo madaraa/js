145587ab8e826b4e0d6652ef29b061371a17dc94
sdt12352 <leecyz@163.com>
2013-11-22 13:38:29 +0800

    TUI测试提交

---
+++ resources/js/TUI.js
@@ -0,0 +1,31 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sdt12352
+ * Date: 13-11-22
+ * Time: 下午1:33
+ * To change this template use File | Settings | File Templates.
+ */
+
+var TUI = (function(){
+
+    var content ={
+        'title':'default',
+        'width' :'400px',
+        'height' :'300px'
+    };
+
+
+    function add(){
+
+    }
+
+    function remove(){
+
+    }
+
+    function TUI(win,obj){
+
+        return {add:add};
+    };
+    return TUI;
+})();
